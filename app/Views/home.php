119d7fc248ebb01a165d84cacb2c97d9c82ee232
seif <seif@seif.com>
2024-06-19 20:20:26 +0300

    Update home.php

--- app/Views/home.php
+++ app/Views/home.php
@@ -1,7 +1,7 @@
 <?php  include(VIEWS.'inc'.DS.'header.php'); ?>
 
 <div class="jumbotron text-center mt-5">
-    <h1 class="display-4">LEARN PHP MVC</h1>
+    <h1 class="display-4">CRUD PHP MVC</h1>
     <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
     <hr class="my-4">
     <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
